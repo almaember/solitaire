d2fcd5eabab2adb0a6269fd412165650056238ff
Tóth Gergely <gergely.zoltan.toth@gmail.com>
2022-09-05 12:20:09 +0200

    cleanup

--- src/app/services/cards.service.ts
+++ src/app/services/cards.service.ts
@@ -1,19 +1,18 @@
-import { Injectable, Sanitizer } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { Card } from 'src/app/model/card';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { Card } from "src/app/model/card";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class CardsService {
+  private baseUrl: string = "http://localhost:3000";
+  private cardsUrl: string = `${this.baseUrl}/cards`;
 
-  private baseUrl: string = 'http://localhost:3000';
-  private cardsUrl: string = `${this.baseUrl}/cards`;  
+  constructor(private httpClient: HttpClient) {}
 
-  constructor(private httpClient : HttpClient) { }
-  
-  get cards (): Observable<Card[]> {
-    return this.httpClient.get<Card[]>(this.cardsUrl, { responseType: 'json' })
-  }  
+  get cards(): Observable<Card[]> {
+    return this.httpClient.get<Card[]>(this.cardsUrl, { responseType: "json" });
+  }
 }
--- src/app/views/game/game.component.ts
+++ src/app/views/game/game.component.ts
@@ -1,67 +1,67 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { CardsService } from 'src/app/services/cards.service';
-import { Card } from 'src/app/model/card';
+import { Component, OnInit } from "@angular/core";
+import { CardsService } from "src/app/services/cards.service";
+import { Card } from "src/app/model/card";
 
 @Component({
-  selector: 'app-game',
-  templateUrl: './game.component.html',
-  styleUrls: ['./game.component.scss']
+  selector: "app-game",
+  templateUrl: "./game.component.html",
+  styleUrls: ["./game.component.scss"],
 })
-export class GameComponent implements OnInit, OnDestroy {
+export class GameComponent implements OnInit {
   stock: Card[];
-  talon: Card[] = []; 
-  tableau: [Card[]] = Array.apply(null, Array(7)).map(function () {return []})
-  foundations: [Card[]] = Array.apply(null, Array(4)).map(function () {return []});
+  talon: Card[] = [];
+  tableau: [Card[]] = Array.apply(null, Array(7)).map(function () {
+    return [];
+  });
+  foundations: [Card[]] = Array.apply(null, Array(4)).map(function () {
+    return [];
+  });
 
-  constructor(private cardService : CardsService) { }
+  constructor(private cardService: CardsService) {}
 
   ngOnInit() {
-    this.cardService.cards.subscribe(deck => {      
+    this.cardService.cards.subscribe((deck) => {
       createTableau(deck);
     });
-    
-    const createTableau = (deck: Card[]) => {     
+
+    const createTableau = (deck: Card[]) => {
       this.stock = deck;
 
       for (let i = 0; i < this.tableau.length; i += 1) {
-        for (let j = 0; j < i + 1 ; j += 1) {         
-
+        for (let j = 0; j < i + 1; j += 1) {
           const dealtCard = this.stock.pop();
 
-          this.tableau[i].push(dealtCard);          
-        };
-      };  
-
-    };    
+          this.tableau[i].push(dealtCard);
+        }
+      }
+    };
   }
 
-  ngOnDestroy() {
-
-  }
-
-  flipACardFromAStock(){   
+  flipACardFromAStock() {
     const flippedCard = this.stock.pop();
     this.talon.push(flippedCard);
   }
 
-  drag(event, card: Card, tableauColumnIndex: number) {    
+  drag(event, card: Card, tableauColumnIndex: number) {
     event.dataTransfer.setData("card", JSON.stringify(card));
     event.dataTransfer.setData("tableauColumnIndex", tableauColumnIndex);
-  }  
+  }
 
-  dropToFoundation(event) {    
-    event.preventDefault(); 
+  dropToFoundation(event) {
+    event.preventDefault();
 
     const card: Card = JSON.parse(event.dataTransfer.getData("card"));
-    const tableauColumnIndex:number = parseInt(event.dataTransfer.getData("tableauColumnIndex"));
-    const foundationIndex: number = parseInt(event.target.id);   
+    const tableauColumnIndex: number = parseInt(
+      event.dataTransfer.getData("tableauColumnIndex")
+    );
+    const foundationIndex: number = parseInt(event.target.id);
 
-    this.validateFoundation(foundationIndex, card, tableauColumnIndex);    
+    this.validateFoundation(foundationIndex, card, tableauColumnIndex);
   }
 
   dropToTableauColumn(event, tableauColumnIndex: number) {
-    event.preventDefault(); 
-    const card: Card = JSON.parse(event.dataTransfer.getData("card"));     
+    event.preventDefault();
+    const card: Card = JSON.parse(event.dataTransfer.getData("card"));
     this.validateTableau(tableauColumnIndex, card);
   }
 
@@ -69,44 +69,51 @@ export class GameComponent implements OnInit, OnDestroy {
     event.preventDefault();
   }
 
-  removeTopCardFromTableauColumn = (tableauColumnIndex: number) => this.tableau[tableauColumnIndex].pop();
+  removeTopCardFromTableauColumn = (tableauColumnIndex: number) =>
+    this.tableau[tableauColumnIndex].pop();
 
-  removeTopCardFromTalon = () => this.talon.pop(); 
-  
-  validateFoundation(foundationIndex: number, card: Card, tableauColumnIndex?: number) {
+  removeTopCardFromTalon = () => this.talon.pop();
 
-    const increasing: boolean = this.foundations[foundationIndex].length + 1 === card.value;
-    const foundationIsNotEmpty: boolean = this.foundations[foundationIndex].length !== 0
+  validateFoundation(
+    foundationIndex: number,
+    card: Card,
+    tableauColumnIndex?: number
+  ) {
+    const increasing: boolean =
+      this.foundations[foundationIndex].length + 1 === card.value;
+    const foundationIsNotEmpty: boolean =
+      this.foundations[foundationIndex].length !== 0;
     let cardTypeIdentical: boolean;
-    
-    if(foundationIsNotEmpty) {
-      cardTypeIdentical = this.foundations[foundationIndex][0].type === card.type;
+
+    if (foundationIsNotEmpty) {
+      cardTypeIdentical =
+        this.foundations[foundationIndex][0].type === card.type;
     } else {
       cardTypeIdentical = true;
     }
 
-    if(increasing && cardTypeIdentical) {
-
-      if(!isNaN(tableauColumnIndex)) {     
-        this.removeTopCardFromTableauColumn(tableauColumnIndex)
-      } else {            
+    if (increasing && cardTypeIdentical) {
+      if (!isNaN(tableauColumnIndex)) {
+        this.removeTopCardFromTableauColumn(tableauColumnIndex);
+      } else {
         this.removeTopCardFromTalon();
-      } 
+      }
 
       this.foundations[foundationIndex].push(card);
     }
   }
 
-  validateTableau(tableauColumnIndex:number, card: Card) {
-    const lastCardInTheTableauColumn = this.tableau[tableauColumnIndex][this.tableau[tableauColumnIndex].length - 1];
-    const decreasing = (lastCardInTheTableauColumn.value - 1) === card.value;
+  validateTableau(tableauColumnIndex: number, card: Card) {
+    const lastCardInTheTableauColumn =
+      this.tableau[tableauColumnIndex][
+        this.tableau[tableauColumnIndex].length - 1
+      ];
+    const decreasing = lastCardInTheTableauColumn.value - 1 === card.value;
     const mismatchColor = lastCardInTheTableauColumn.color !== card.color;
 
-    if( decreasing && mismatchColor) {    
+    if (decreasing && mismatchColor) {
       this.removeTopCardFromTalon();
       this.tableau[tableauColumnIndex].push(card);
-    } 
+    }
   }
-
-
 }
